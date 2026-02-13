55213b8b3cfe5ba8bc5374bc33b4b9999ec988bf
HiGarfield <32226909+HiGarfield@users.noreply.github.com>
2026-02-14 05:05:26 +0800

    Fix run_command process/status handling

--- common.cpp
+++ common.cpp
@@ -828,18 +828,14 @@ vector<vector<string> > string_to_vec2(const char *s) {
 }
 int read_file(const char *file, string &output) {
     const int max_len = 3 * 1024 * 1024;
-    // static char buf[max_len+100];
-    string buf0;
-    buf0.reserve(max_len + 200);
-    char *buf = (char *)buf0.c_str();
-    buf[max_len] = 0;
-    // buf[sizeof(buf)-1]=0;
+    vector<char> buf(max_len + 1, 0);
     int fd = open(file, O_RDONLY);
     if (fd == -1) {
         mylog(log_error, "read_file %s fail\n", file);
         return -1;
     }
-    int len = read(fd, buf, max_len);
+    int len = read(fd, buf.data(), max_len);
+    close(fd);
     if (len == max_len) {
         buf[0] = 0;
         mylog(log_error, "%s too long,buf not large enough\n", file);
@@ -850,7 +846,7 @@ int read_file(const char *file, string &output) {
         return -3;
     } else {
         buf[len] = 0;
-        output = buf;
+        output = buf.data();
     }
     return 0;
 }
@@ -880,9 +876,10 @@ int run_command(string command0, char *&output, int flag) {
         return -1;
     }
 
-    int len = fread(buf, 1024 * 1024, 1, in);
+    size_t len = fread(buf, 1, 1024 * 1024, in);
     if (len == 1024 * 1024) {
         buf[0] = 0;
+        pclose(in);
         mylog(level, "too long,buf not larger enough\n");
         return -2;
     } else {
@@ -890,6 +887,7 @@ int run_command(string command0, char *&output, int flag) {
     }
     int ret;
     if ((ret = ferror(in))) {
+        pclose(in);
         mylog(level, "command %s fread failed,ferror return value %d \n", command, ret);
         return -3;
     }
@@ -897,10 +895,14 @@ int run_command(string command0, char *&output, int flag) {
     output = buf;
     ret = pclose(in);
 
-    int ret2 = WEXITSTATUS(ret);
+    if (ret == -1) {
+        mylog(level, "commnad %s ,pclose failed,errno :%s \n", command, strerror(errno));
+        return -4;
+    }
 
-    if (ret != 0 || ret2 != 0) {
-        mylog(level, "commnad %s ,pclose returned %d ,WEXITSTATUS %d,errnor :%s \n", command, ret, ret2, strerror(errno));
+    if (!WIFEXITED(ret) || WEXITSTATUS(ret) != 0) {
+        mylog(level, "commnad %s ,pclose returned %d ,WIFEXITED %d,WEXITSTATUS %d,errnor :%s \n", command, ret, WIFEXITED(ret),
+              WIFEXITED(ret) ? WEXITSTATUS(ret) : -1, strerror(errno));
         return -4;
     }
 
